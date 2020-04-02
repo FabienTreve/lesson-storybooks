5a3385ddfc02644b60e08366fb2637ee05033c07
FabienTreve <trevefabien@gmail.com>
2020-04-02 10:11:58 +0200

    auth and save user into db

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const passport = require('passport');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+
+// Load user Model
+require('./models/User');
 
 // Passport Config
 require('./config/passport')(passport);
@@ -28,6 +33,24 @@ var app = express();
 app.get('/', (req, res) => {
     res.send('OK');
 });
+
+// Cookie and session middlewares
+app.use(cookieParser());
+app.use(session({
+    secret: 'secret',
+    resave: false,
+    saveUninitialized: false
+}))
+
+// Passport Middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Set global vars
+app.use((req, res, next) => {
+    res.locals.user = req.user || null;
+    next();
+})
 
 // Use Routes
 app.use('/auth', auth);

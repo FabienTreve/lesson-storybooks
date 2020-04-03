53541e21808858c2fbd11d3d5ad112ece58fac4c
FabienTreve <trevefabien@gmail.com>
2020-04-03 12:14:57 +0200

    handlebars, welcome, main, navbar

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const exphbs = require('express-handlebars');
 const mongoose = require('mongoose');
 const passport = require('passport');
 const cookieParser = require('cookie-parser');
@@ -11,6 +12,7 @@ require('./models/User');
 require('./config/passport')(passport);
 
 // Load Routes
+const index = require('./routes/index');
 const auth = require('./routes/auth');
 
 // Load keys
@@ -30,9 +32,11 @@ mongoose.connect(keys.mongoURI, {
 
 var app = express();
 
-app.get('/', (req, res) => {
-    res.send('OK');
-});
+// Handlebars Middleware
+app.engine('handlebars', exphbs({
+    defaultLayout: 'main'
+}));
+app.set('view engine', 'handlebars')
 
 // Cookie and session middlewares
 app.use(cookieParser());
@@ -50,9 +54,10 @@ app.use(passport.session());
 app.use((req, res, next) => {
     res.locals.user = req.user || null;
     next();
-})
+});
 
 // Use Routes
+app.use('/', index);
 app.use('/auth', auth);
 
 const port = process.env.PORT || 5000;

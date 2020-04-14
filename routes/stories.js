421b3e12ce63266481ca3a5847cfcda1f8020375
FabienTreve <trevefabien@gmail.com>
2020-04-14 12:55:52 +0200

    show page, dashboard page, edit form

--- routes/stories.js
+++ routes/stories.js
@@ -16,9 +16,34 @@ router.get('/', (req, res) => {
         });
 });
 
+//Show Single Story
+router.get('/show/:id', (req, res) => {
+    Story.findOne({
+        _id: req.params.id
+    })
+    .populate('user')
+    .then(story => {
+        res.render('stories/show', {
+            story: story
+        });
+    });
+});
+
 // Add Story Form
 router.get('/add', ensureAuthenticated, (req, res) => {
     res.render('stories/add');
+});
+
+// Edit Story Form
+router.get('/edit/:id', ensureAuthenticated, (req, res) => {
+    Story.findOne({
+        _id: req.params.id
+    })
+    .then(story => {
+        res.render('stories/edit', {
+            story: story
+        });
+    });
 });
 
 // Process Add Story
